8640665db633d194811b836840f84af69ff84a4c
Sanskriti Kushik <sanskritikushik23@gmail.com>
2022-08-10 12:03:54 +0530

    Update find-the-duplicate-number.cpp

--- Arrays/Easy/find-the-duplicate-number.cpp
+++ Arrays/Easy/find-the-duplicate-number.cpp
@@ -1,1 +1,13 @@
-zchckzjhkjzh
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(auto x:nums)
+            map[x]++;
+        for(auto x:map){
+            if(x.second>1)
+                return x.first;
+        }
+        return 0;
+    }
+};
